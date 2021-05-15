95fb70d620415e50df54530db0b455ec4dde4990
Faisal Khalid <faisalkhalid246@gmail.com>
2021-05-15 17:57:31 +0000

    Added some long mode support

--- boot/boot.s
+++ boot/boot.s
@@ -1,3 +1,5 @@
+.intel_syntax noprefix
+
 /*
 The multiboot header requires a 'magic number', some flags, and a checksum
 because of the multiboot standard. The bootloader will the first 8kb to see
@@ -32,7 +34,10 @@ _start:
 	The esp register generally holds the top of the stack. We are setting that
 	up here.
 	*/
-	mov $stack_top, %esp
+	mov stack_top, %esp
+
+	call check_CPUID
+	call check_long_mode
  
 	/*
 	Finally call our kernel
@@ -40,3 +45,82 @@ _start:
 	call kernel_main
 	hlt
  
+/*
+CPUID allows us to see the details of our processor, including support for long mode. Before we
+can use that instruction, we need to confirm that processor allows using it. To do so, we check
+our flags to see if ID bit (number 21) can be flipped. If it can, we have CPUID support.
+*/
+check_CPUID:
+	pushfd        		# Saves the flags
+	pop eax       		# Stores them onto the register
+	mov ecx, eax  		# Store additional copy onto ecx register
+
+	xor eax, 1 << 21    # Flip bit number 21 on eax - this is the ID bit
+
+	push eax      		# Push eax back onto the stack
+    popfd				# Now pop it but back into the flags register
+
+	pushfd				# Save flag on stack again (this time with the modified bit)
+	pop eax				# Store modified flag into the register again
+
+	push ecx			# Push the correct flags back onto the stack (UN-modifed)
+	popfd				# Store the correct flags back onto the flags register
+
+	# Now compare the flags
+	xor eax, ecx
+	jz no_CPUID
+    ret
+
+/*
+This checks for long mode by first checking for the processor's extended function support. If
+it does find the support, it then checks bit 29 in the d register to see if there is long mode
+support.
+*/
+check_long_mode:
+	# Checks for extended functions
+	mov eax, 0x80000000    # Set the A-register to 0x80000000
+    cpuid                  # CPU identification will save maximum input value for CPUID info
+    cmp eax, 0x80000001    # Check to see if it is greater than 0x80000000
+    jb no_long_mode        # There is no long mode if it is less than 0x80000000
+
+	# Checks for long mode
+	mov eax, 0x80000001    # Set the A-register to 0x80000001.
+    cpuid                  # CPU identification.
+    test edx, 1 << 29      # Test if the LM-bit, which is bit 29, is set in the D-register.
+    jz no_long_mode        # They aren't, there is no long mode.
+
+
+/*
+This subroutine sets the error number to 2 and then calls the print error routine.
+An error of 2 means that we did not find long mode support.
+*/
+no_long_mode:
+	mov al, 0x32
+	call print_error
+	ret
+
+/*
+This subroutine sets the error number to 1 and then calls the print error routine.
+An error of 1 means that we could not find CPUID support.
+*/
+no_CPUID:
+	mov al, 0x31
+	call print_error
+	ret
+
+/*
+This little subroutine will print out an error for us whenever there is an issue. For
+reference, the terminal buffer begins at the address 0xB8000.
+*/
+print_error:
+	mov byte ptr [0xB8000], 0x45 # E
+	mov byte ptr [0xB8002], 0x72 # r
+	mov byte ptr [0xB8004], 0x72 # r
+	mov byte ptr [0xB8006], 0x6f # o
+	mov byte ptr [0xB8008], 0x72 # r
+	mov byte ptr [0xB800a], 0x3a # :
+	mov byte ptr [0xB800c], al
+	hlt
+
+
+
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
-myos.bin : boot.o kernel.o linker.ld
-	ld -T linker.ld -m elf_i386 -o myos.bin kernel.o boot.o
+myos.bin : ./boot/boot.o ./kernel/kernel.o ./boot/linker.ld
+	ld -T ./boot/linker.ld -m elf_i386 -o myos.bin ./kernel/kernel.o ./boot/boot.o
 
-kernel.o : kernel.c
-	gcc -c kernel.c -o kernel.o -ffreestanding -nostdlib -m32
+./kernel/kernel.o : ./kernel/kernel.c
+	gcc -c ./kernel/kernel.c -o ./kernel/kernel.o -ffreestanding -nostdlib -m32
 
-boot.o : boot.s
-	gcc -c boot.s boot.o -m32
+./boot/boot.o : ./boot/boot.s
+	gcc -c ./boot/boot.s -o ./boot/boot.o -m32
